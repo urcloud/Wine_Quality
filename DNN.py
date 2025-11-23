2876cd0fb3afcb506e85b210af638f8fc2f1911e
이유정 <epoint8989@gmail.com>
2025-11-23 15:38:56 +0900

    3주차 코드 초안

--- DNN.py
+++
@@ -1,8 +0,0 @@
-# 비선형 딥러닝 모델 DNN 구현 예정
-# 선형 모델에 비해 복잡한 패턴 학습 가능
-# Neural Networks (DNN, MLP) + 활성화 함수(ReLU, sigmoid 등) 사용하면 비선형
-
-# 이 아래는 머신러닝
-# Decision Tree Regression
-# Random Forest Regression
-# k-Nearest Neighbors Regression
